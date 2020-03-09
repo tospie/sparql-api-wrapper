153e85d18215d519c08cbe66e505b9d71980049f
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2020-03-09 12:05:43 +0100

    Using actual resultwriter classes in Serializer; TODO: Move this entirely to processor

--- sparql-api-wrapper/src/main/java/de/dfki/asr/smartmaas/json/sparql/rdf/QueryResultSerializer.java
+++ sparql-api-wrapper/src/main/java/de/dfki/asr/smartmaas/json/sparql/rdf/QueryResultSerializer.java
@@ -10,7 +10,10 @@ import java.io.OutputStream;
 import org.eclipse.rdf4j.query.BindingSet;
 import org.eclipse.rdf4j.query.GraphQueryResult;
 import org.eclipse.rdf4j.query.QueryResult;
+import org.eclipse.rdf4j.query.QueryResults;
 import org.eclipse.rdf4j.query.TupleQueryResult;
+import org.eclipse.rdf4j.query.resultio.TupleQueryResultWriter;
+import org.eclipse.rdf4j.query.resultio.sparqljson.SPARQLResultsJSONWriter;
 import org.eclipse.rdf4j.rio.RDFFormat;
 import org.eclipse.rdf4j.rio.RDFWriter;
 import org.eclipse.rdf4j.rio.Rio;
@@ -21,11 +24,16 @@ import org.eclipse.rdf4j.rio.Rio;
  */
 public class QueryResultSerializer {
 
-	public <T extends QueryResult> String serialize(T result) {
+	public <T extends QueryResult> byte[] serialize(T result) {
 		if (result instanceof TupleQueryResult) {
-			return serialize((TupleQueryResult) result);
+			ByteArrayOutputStream output = new ByteArrayOutputStream();
+			TupleQueryResultWriter writer = new SPARQLResultsJSONWriter(output);
+			QueryResults.report((TupleQueryResult) result, writer);
+
+			return output.toByteArray();
+			//return serialize((TupleQueryResult) result);
 		} else if (result instanceof GraphQueryResult) {
-			return serialize((GraphQueryResult) result);
+			return new byte[0]; //serialize((GraphQueryResult) result);
 		}
 
 		throw new UnsupportedOperationException("Not Yet Implemented");
